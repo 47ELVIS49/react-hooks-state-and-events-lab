9a414090df5e434a1815b5e84e437dbd6b70c1bd
Elvis Momanyi <elvismomanyi23@gmail.com>
2024-02-05 15:44:20 +0300

    done with the lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,24 +1,32 @@
-import React from "react";
-import Item from "./Item";
+import React, { useState } from 'react';
 
-function ShoppingList({ items }) {
+const ShoppingList = ({ items }) => {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const filteredItems =
+    selectedCategory === 'All'
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
+
   return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+    <div>
+      <select onChange={handleCategoryChange} value={selectedCategory}>
+        <option value="All">All</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Produce">Produce</option>
+        <option value="Dessert">Dessert</option>
+      </select>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredItems.map((item) => (
+          <li key={item.id}>{item.name}</li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default ShoppingList;
---
+++ src/components/data.js
@@ -0,0 +1,36 @@
+const data = {
+    name: "Your Blog Name",
+    image: "https://via.placeholder.com/215",
+    about: "About your blog. This is a sample about text.",
+    posts: [
+      {
+        id: 1,
+        title: "Components 101",
+        date: "December 15, 2020",
+        preview: "Setting up the building blocks of your site",
+      },
+      {
+        id: 2,
+        title: "React Data Flow",
+        date: "December 11, 2020",
+        preview: "Passing props is never passé",
+      },
+      {
+        id: 3,
+        title: "Function Components vs Class Components",
+        date: "December 10, 2020",
+        preview: "React, meet OOJS.",
+      },
+    ],
+    items: [
+      { id: 1, name: "Milk", category: "Dairy" },
+      { id: 2, name: "Yogurt", category: "Dairy" },
+      { id: 3, name: "Pomegranate", category: "Produce" },
+      { id: 4, name: "Lettuce", category: "Produce" },
+      { id: 5, name: "String Cheese", category: "Dairy" },
+      { id: 6, name: "Cookies", category: "Dessert" },
+    ],
+  };
+  
+  export default data;
+  
